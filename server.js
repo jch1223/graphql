8fd54558954e8cd7bd9bd56a03e79552c4510d62
jch1223 <ico1828@gmail.com>
2023-06-22 00:28:30 +0900

    feat: rest api를 qraphql로 랩핑

--- server.js
+++ server.js
@@ -1,98 +1,137 @@
 import { ApolloServer, gql } from "apollo-server";
+import fetch from "node-fetch";
 
-const tweets = [
+let tweets = [
   {
     id: "1",
-    text: "Hello World",
-    userId: "1",
+    text: "first one!",
+    userId: "2",
   },
   {
     id: "2",
-    text: "Bye World",
+    text: "second one",
     userId: "1",
-  },
-  {
-    id: "3",
-    text: "Hello Again",
-    userId: "2",
   },
 ];
 
-const users = [
+let users = [
   {
     id: "1",
-    firstName: "John",
-    lastName: "Doe",
+    firstName: "nico",
+    lastName: "las",
   },
   {
     id: "2",
-    firstName: "Jane",
-    lastName: "Doe",
+    firstName: "Elon",
+    lastName: "Mask",
   },
 ];
 
 const typeDefs = gql`
   type User {
-    id: ID
-    firstName: String
-    lastName: String
-    fullName: String
+    id: ID!
+    firstName: String!
+    lastName: String!
+    """
+    Is the sum of firstName + lastName as a string
+    """
+    fullName: String!
   }
   """
-  A Tweet object
+  Tweet object represents a resource for  a Tweet
   """
   type Tweet {
-    id: ID
-    text: String
-    author: User!
+    id: ID!
+    text: String!
+    author: User
   }
   type Query {
-    allTweets: [Tweet!]!
+    allMovies: [Movie!]!
     allUsers: [User!]!
-    tweet(id: ID): Tweet
-    ping: String
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+    movie(id: String!): Movie
   }
   type Mutation {
-    postTweet(text: String, userId: ID): Tweet
-    deleteTweet(id: ID): Boolean
+    postTweet(text: String!, userId: ID!): Tweet!
+    """
+    Deletes a Tweet if found, else returns false
+    """
+    deleteTweet(id: ID!): Boolean!
+  }
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String!
+    title_long: String!
+    slug: String!
+    year: Int!
+    rating: Float!
+    runtime: Float!
+    genres: [String]!
+    summary: String
+    description_full: String!
+    synopsis: String
+    yt_trailer_code: String!
+    language: String!
+    background_image: String!
+    background_image_original: String!
+    small_cover_image: String!
+    medium_cover_image: String!
+    large_cover_image: String!
   }
 `;
 
 const resolvers = {
   Query: {
-    allTweets: (parent, args, context, info) => {
+    allTweets() {
       return tweets;
     },
-    tweet: (parent, args, context, info) => {
-      return tweets.find((tweet) => tweet.id === args.id);
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
     },
-    allUsers: (parent, args, context, info) => {
+    allUsers() {
+      console.log("allUsers called!");
       return users;
+    },
+    allMovies() {
+      return fetch("https://yts.mx/api/v2/list_movies.json")
+        .then((r) => r.json())
+        .then((json) => json.data.movies);
+    },
+    movie(_, { id }) {
+      return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        .then((r) => r.json())
+        .then((json) => json.data.movie);
     },
   },
   Mutation: {
-    postTweet: (parent, args, context, info) => {
-      const tweet = {
-        id: String(tweets.length + 1),
-        text: args.text,
-        authorId: args.userId,
+    postTweet(_, { text, userId }) {
+      const newTweet = {
+        id: tweets.length + 1,
+        text,
+        userId,
       };
-      tweets.push(tweet);
-      return tweet;
+      tweets.push(newTweet);
+      return newTweet;
     },
-    deleteTweet: (parent, args, context, info) => {
-      tweets = tweets.filter((tweet) => tweet.id !== args.id);
+    deleteTweet(_, { id }) {
+      const tweet = tweets.find((tweet) => tweet.id === id);
+      if (!tweet) return false;
+      tweets = tweets.filter((tweet) => tweet.id !== id);
       return true;
     },
   },
   User: {
-    fullName: (parent, args, context, info) => {
-      return `${parent.firstName} ${parent.lastName}`;
+    fullName({ firstName, lastName }) {
+      return `${firstName} ${lastName}`;
     },
   },
   Tweet: {
-    author: (parent, args, context, info) => {
-      return users.find((user) => user.id === parent.userId);
+    author({ userId }) {
+      return users.find((user) => user.id === userId);
     },
   },
 };
@@ -100,5 +139,5 @@ const resolvers = {
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
-  console.log(url);
+  console.log(`Running on ${url}`);
 });
